695a30d4f3cdb5a6c42283443923e6267ec0f7ff
x1nax <53822451+x1nax@users.noreply.github.com>
2020-11-07 16:25:08 +0700

    Add files via upload

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -5,16 +5,15 @@ import { IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import styles from "./ItemList.module.css"
 
-const ItemList = ({items, onClickDone, onClickDelete}) => (<ul className={styles.list}>
-  {items.map(item => <li key={item.value} className={styles.item}>
+const ItemList = ({items, onClickDone, onClickDelete, btnisActive}) => (<ul className={styles.list}>
+  {items.map(item => <li key={item.id} className={styles.item}>
     <Checkbox
         color="default"
         inputProps={{ 'aria-label': 'checkbox with default color' }}
         onClick={() => onClickDone(item.id)}
     />
-    <Item 
-    value={item.value} isDone={item.isDone}/>
-    <IconButton aria-label="delete" onClick={()=> onClickDelete(item.id)}>
+    <Item value={item.value} isDone={item.isDone}/>
+    <IconButton aria-label="delete"  onClick={()=> onClickDelete(item.id)}>
         <DeleteIcon/>
     </IconButton>
   </li>)}
