97494c9b7cf2a965d01bc810e2a40b80254b1ac8
x1nax <53822451+x1nax@users.noreply.github.com>
2020-12-02 03:09:00 +0700

    Add files via upload

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {Octokit} from '@octokit/rest';
 
-const About = () => (
-  <div>
-    В работе
-  </div>
-);
+const octokit = new Octokit ();
+
+class About extends React.Component{
+  state = {
+    isLoading: true,
+    repoList: [],
+    fetchFailure: false,
+    err: {}
+  }
+
+  componentDidMount() {
+    octokit.repos.listForUser({
+      username: 'x1nax'
+    }).then(({data}) => {
+      this.setState({
+        repoList: data,
+        isLoading: false
+      });
+    }).catch(error => {
+      this.setState({
+        fetchFailure: true,
+        err: error
+      });
+    });
+  }
+
+
+  render() { 
+    const {isLoading, repoList, err, fetchFailure} = this.state;
+
+    return (
+      <CardContent>
+       <h1>{isLoading ? <CircularProgress /> : 'обо мне'}</h1>
+       {fetchFailure && <div>{err.message}</div>}
+       {!isLoading && <div>
+        <img src={repoList.map(repo => (repo.owner.avatar_url))}></img>
+        <ol>
+         { repoList.map(repo => (<li key={repo.id}>
+          <h1></h1>
+           <a href={repo.svn_url}>{repo.name}</a>
+         </li>)) }
+       </ol>
+       </div>
+       }
+      </CardContent>
+      
+    );
+  }
+}
+
+
 
 export default About;
